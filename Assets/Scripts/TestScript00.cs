44e431888e3ba388d08f152246fc4d64d990ee3b
Bernd Leister <surlix313@gmail.com>
2021-11-01 11:05:04 +0100

    create dev branch

---
+++ Assets/Scripts/TestScript00.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript00 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.log("test");
+        //kommentar hier, ignorier mich nicht ;D
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
