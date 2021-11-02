92a5d1960d3c45eaac35eab12f0d491433fc7b36
Rahil Kaiser <rahil.kaiser@s2017.tu-chemnitz.de>
2021-11-02 03:06:46 +0100

    Added Simple Menues with navigation, added Buttons,

---
+++ Assets/Scripts/MenuScreen/Navigation.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Navigation : MonoBehaviour
+{
+    /** Loads the Scene by given BuildIndex
+     * 
+     **/ 
+    public void LoadSceneByIndex(int buildIndex)
+    {
+        SceneManager.LoadScene(buildIndex);
+    }
+
+    /** Loads the Scene by BuildIndex
+     * 
+     **/ 
+    public void LoadNextScene()
+    {
+        int currentIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextIndex = currentIndex + 1;
+
+        if(nextIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            this.LoadSceneByIndex(nextIndex);
+        }
+        else
+        {
+            Debug.Log("BuildIndex is out of Range" 
+                + "Tried to Load SceneIndex = " + nextIndex 
+                + " ... Max reachable index is " + (SceneManager.sceneCountInBuildSettings - 1));
+        }
+    }
+
+    /** Quits the Game
+     * 
+     **/ 
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+
+    /** Enables MenuScreen
+     *
+     **/
+    public void EnableMenu(GameObject menuToEnable)
+    {
+        menuToEnable.gameObject.SetActive(true);
+    }
+
+    /** Disables MenuScreen
+     * 
+     **/ 
+    public void DisableMenu(GameObject menuToDisable)
+    {
+        menuToDisable.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/MenuScreen/UserConfiguration.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum UserRole
+{
+    LOBBY_CREATOR,
+    LOBBY_JOINER,   //People who just join the lobby
+    SPECTATOR,      //passive people are automatically Spectator
+}
+
+public class UserConfiguration : MonoBehaviour
+{
+    private bool isActive = false;
+    private Vector3 lightDir = new Vector3(0, 0, 0);
+    private UserRole role = UserRole.SPECTATOR;
+
+    public void setActive()
+    {
+        isActive = true;
+    }
+    public void setPassive()
+    {
+        isActive = false;
+    }
+    public bool getIsActive()
+    {
+        return isActive;
+    }
+
+    public void setLightDir(Vector3 lightDir)
+    {
+        this.lightDir = lightDir;
+    }
+    public Vector3 getLightDir()
+    {
+        return this.lightDir;
+    }
+
+    public void setUserToLobbyCreator()
+    {
+        this.role = UserRole.LOBBY_CREATOR;
+    }
+    public void setUserRoleToLobbyJoiner()
+    {
+        this.role = UserRole.LOBBY_JOINER;
+    }
+    public void setUserRoleToSpectator()
+    {
+        this.role = UserRole.SPECTATOR;
+    }
+
+
+}
