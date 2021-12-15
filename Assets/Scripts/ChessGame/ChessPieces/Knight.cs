52e565e00a0306fb5ad74c894f8375bf5de3b48b
ESX1234 <ESX1234@DESKTOP-KUP00MC>
2021-12-15 22:07:18 +0100

    Implemented moveLogic for Knight and Queen

--- Assets/Scripts/ChessGame/ChessPieces/Knight.cs
+++ Assets/Scripts/ChessGame/ChessPieces/Knight.cs
@@ -4,4 +4,94 @@ using UnityEngine;
 
 public class Knight : ChessPiece
 {
+    public override List<Vector2Int> GetPossibleMoves(ref ChessPiece[,] chessPieceMap, int tileCountX, int tileCountY)
+    {
+        List<Vector2Int> possibleMoves = new List<Vector2Int>();
+
+        //1 forward
+        Vector2Int currentPos = ChessGameUtil.floorToIntVector2Int(this.currentPosition);
+
+        //2 Steps North
+        if(currentPos.y + 2 < tileCountY)
+        {
+            if(currentPos.x + 1 < tileCountX 
+                && (chessPieceMap[currentPos.x + 1, currentPos.y + 2] == null 
+                    || chessPieceMap[currentPos.x + 1, currentPos.y + 2].team != this.team)
+                )
+            {
+                possibleMoves.Add(new Vector2Int(currentPos.x +1, currentPos.y +2));
+            }
+            
+            if(currentPos.x -1 >= 0
+                && (chessPieceMap[currentPos.x - 1, currentPos.y + 2] == null
+                    || chessPieceMap[currentPos.x - 1, currentPos.y + 2].team != this.team)
+                )
+            {
+                possibleMoves.Add(new Vector2Int(currentPos.x -1, currentPos.y +2));
+            }
+        }
+
+        //2 Steps South
+        if (currentPos.y - 2 >= 0)
+        {
+            if (currentPos.x + 1 < tileCountX
+                && (chessPieceMap[currentPos.x + 1, currentPos.y -2] == null
+                    || chessPieceMap[currentPos.x + 1, currentPos.y -2].team != this.team)
+                )
+            {
+                possibleMoves.Add(new Vector2Int(currentPos.x + 1, currentPos.y - 2));
+            }
+
+            if (currentPos.x - 1 >= 0
+                && (chessPieceMap[currentPos.x - 1, currentPos.y - 2] == null
+                    || chessPieceMap[currentPos.x - 1, currentPos.y - 2].team != this.team)
+                )
+            {
+                possibleMoves.Add(new Vector2Int(currentPos.x - 1, currentPos.y - 2));
+            }
+        }
+        
+        
+        //2 Steps West
+        if (currentPos.x - 2 >= 0)
+        {
+            if (currentPos.y + 1 < tileCountY
+                && (chessPieceMap[currentPos.x -2 , currentPos.y +1 ] == null
+                    || chessPieceMap[currentPos.x -2, currentPos.y +1].team != this.team)
+                )
+            {
+                possibleMoves.Add(new Vector2Int(currentPos.x - 2, currentPos.y + 1));
+            }
+
+            if (currentPos.y - 1 >= 0
+                && (chessPieceMap[currentPos.x - 2, currentPos.y - 1] == null
+                    || chessPieceMap[currentPos.x - 2, currentPos.y - 1].team != this.team)
+                )
+            {
+                possibleMoves.Add(new Vector2Int(currentPos.x - 2, currentPos.y - 1));
+            }
+        }
+
+        //2 Steps East
+        if (currentPos.x + 2 < tileCountX)
+        {
+            if (currentPos.y + 1 < tileCountY
+                && (chessPieceMap[currentPos.x + 2, currentPos.y + 1] == null
+                    || chessPieceMap[currentPos.x + 2, currentPos.y + 1].team != this.team)
+                )
+            {
+                possibleMoves.Add(new Vector2Int(currentPos.x + 2, currentPos.y + 1));
+            }
+
+            if (currentPos.y - 1 >= 0
+                && (chessPieceMap[currentPos.x + 2, currentPos.y - 1] == null
+                    || chessPieceMap[currentPos.x + 2, currentPos.y - 1].team != this.team)
+                )
+            {
+                possibleMoves.Add(new Vector2Int(currentPos.x + 2, currentPos.y - 1));
+            }
+        }
+
+        return possibleMoves;
+    }
 }
--- Assets/Scripts/ChessGame/ChessPieces/Queen.cs
+++ Assets/Scripts/ChessGame/ChessPieces/Queen.cs
@@ -4,4 +4,188 @@ using UnityEngine;
 
 public class Queen : ChessPiece
 {
+    public override List<Vector2Int> GetPossibleMoves(ref ChessPiece[,] chessPieceMap, int tileCountX, int tileCountY)
+    {
+        List<Vector2Int> possibleMoves = new List<Vector2Int>();
+
+        Vector2Int currentPos = ChessGameUtil.floorToIntVector2Int(this.currentPosition);
+
+        //North
+        for (int i = currentPos.y + 1; i < tileCountY; i++)
+        {
+            //Break on friend
+            if (chessPieceMap[currentPos.x, i] != null
+                && chessPieceMap[currentPos.x, i].team == this.team)
+            {
+                break;
+            }
+
+            //Break on enemy
+            if (chessPieceMap[currentPos.x, i] != null
+                && chessPieceMap[currentPos.x, i].team != this.team)
+            {
+                possibleMoves.Add(new Vector2Int(currentPos.x, i));
+                break;
+            }
+
+            possibleMoves.Add(new Vector2Int(currentPos.x, i));
+        }
+
+        //South
+        for (int i = currentPos.y - 1; i >= 0; i--)
+        {
+            //Break on friend
+            if (chessPieceMap[currentPos.x, i] != null
+                && chessPieceMap[currentPos.x, i].team == this.team)
+            {
+                break;
+            }
+
+            //Break on enemy
+            if (chessPieceMap[currentPos.x, i] != null
+                && chessPieceMap[currentPos.x, i].team != this.team)
+            {
+                possibleMoves.Add(new Vector2Int(currentPos.x, i));
+                break;
+            }
+
+            possibleMoves.Add(new Vector2Int(currentPos.x, i));
+        }
+
+        //East
+        for (int i = currentPos.x + 1; i < tileCountX; i++)
+        {
+            //break on friend
+            if (chessPieceMap[i, currentPos.y] != null
+                && chessPieceMap[i, currentPos.y].team == this.team)
+            {
+                break;
+            }
+
+            //Break on enemy
+            if (chessPieceMap[i, currentPos.y] != null
+                && chessPieceMap[i, currentPos.y].team != this.team)
+            {
+                possibleMoves.Add(new Vector2Int(i, currentPos.y));
+                break;
+            }
+
+            possibleMoves.Add(new Vector2Int(i, currentPos.y));
+        }
+
+        //West
+        for (int i = currentPos.x - 1; i >= 0; i--)
+        {
+            //break on friend
+            if (chessPieceMap[i, currentPos.y] != null
+                && chessPieceMap[i, currentPos.y].team == this.team)
+            {
+                break;
+            }
+
+            //Break on enemy
+            if (chessPieceMap[i, currentPos.y] != null
+                && chessPieceMap[i, currentPos.y].team != this.team)
+            {
+                possibleMoves.Add(new Vector2Int(i, currentPos.y));
+                break;
+            }
+
+            possibleMoves.Add(new Vector2Int(i, currentPos.y));
+        }
+
+        int j = 1;
+        //NorthEast
+        while ((currentPos.x + j) < tileCountX && (currentPos.y + j) < tileCountY)
+        {
+            //Break on friend
+            if (chessPieceMap[currentPos.x + j, currentPos.y + j] != null
+                && chessPieceMap[currentPos.x + j, currentPos.y + j].team == this.team)
+            {
+                break;
+            }
+
+            //Break on enemy
+            if (chessPieceMap[currentPos.x + j, currentPos.y + j] != null
+                && chessPieceMap[currentPos.x + j, currentPos.y + j].team != this.team)
+            {
+                possibleMoves.Add(new Vector2Int(currentPos.x + j, currentPos.y + j));
+                break;
+            }
+            possibleMoves.Add(new Vector2Int(currentPos.x + j, currentPos.y + j));
+
+            j++;
+        }
+
+        //NorthWest
+        j = 1;
+        while ((currentPos.x - j) >= 0 && (currentPos.y + j) < tileCountY)
+        {
+            //Break on friend
+            if (chessPieceMap[currentPos.x - j, currentPos.y + j] != null
+                && chessPieceMap[currentPos.x - j, currentPos.y + j].team == this.team)
+            {
+                break;
+            }
+
+            //Break on enemy
+            if (chessPieceMap[currentPos.x - j, currentPos.y + j] != null
+                && chessPieceMap[currentPos.x - j, currentPos.y + j].team != this.team)
+            {
+                possibleMoves.Add(new Vector2Int(currentPos.x - j, currentPos.y + j));
+                break;
+            }
+            possibleMoves.Add(new Vector2Int(currentPos.x - j, currentPos.y + j));
+
+            j++;
+        }
+
+        //SouthWest
+        j = 1;
+        while ((currentPos.x - j) >= 0 && (currentPos.y - j) >= 0)
+        {
+            //Break on friend
+            if (chessPieceMap[currentPos.x - j, currentPos.y - j] != null
+                && chessPieceMap[currentPos.x - j, currentPos.y - j].team == this.team)
+            {
+                break;
+            }
+
+            //Break on enemy
+            if (chessPieceMap[currentPos.x - j, currentPos.y - j] != null
+                && chessPieceMap[currentPos.x - j, currentPos.y - j].team != this.team)
+            {
+                possibleMoves.Add(new Vector2Int(currentPos.x - j, currentPos.y - j));
+                break;
+            }
+            possibleMoves.Add(new Vector2Int(currentPos.x - j, currentPos.y - j));
+
+            j++;
+        }
+
+        //SouthEast
+        j = 1;
+        while ((currentPos.x + j) < tileCountX && (currentPos.y - j) >= 0)
+        {
+            //Break on friend
+            if (chessPieceMap[currentPos.x + j, currentPos.y - j] != null
+                && chessPieceMap[currentPos.x + j, currentPos.y - j].team == this.team)
+            {
+                break;
+            }
+
+            //Break on enemy
+            if (chessPieceMap[currentPos.x + j, currentPos.y - j] != null
+                && chessPieceMap[currentPos.x + j, currentPos.y - j].team != this.team)
+            {
+                possibleMoves.Add(new Vector2Int(currentPos.x + j, currentPos.y - j));
+                break;
+            }
+            possibleMoves.Add(new Vector2Int(currentPos.x + j, currentPos.y - j));
+
+            j++;
+        }
+
+        return possibleMoves;
+    }
 }
