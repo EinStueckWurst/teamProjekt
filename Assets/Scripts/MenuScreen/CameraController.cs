7a77c69858623a6597dd8f13ac0388ba393eb2c1
Bernd Leister <surlix313@gmail.com>
2021-12-22 17:55:37 +0100

    loading of custom image (in cameracontroller.cs: StartTakingPhotoCoroutine)

--- Assets/Scripts/MenuScreen/CameraController.cs
+++ Assets/Scripts/MenuScreen/CameraController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
 
 public class CameraController : MonoBehaviour
 {
@@ -28,19 +29,19 @@ public class CameraController : MonoBehaviour
     private void InitializeCamera()
     {
         WebCamDevice[] devices = WebCamTexture.devices;
-        if (devices.Length == 0)    return;
+        if (devices.Length == 0) return;
 
         for (int i = 0; i < devices.Length; i++)
         {
             var deviceCam = devices[i];
-            
+
             if (SystemInfo.deviceType == DeviceType.Desktop && deviceCam.isFrontFacing)
             {
                 float aspect = (float)(Screen.width / Screen.height);
-                camera = new WebCamTexture(deviceCam.name, Mathf.FloorToInt( aspect * 1000), 1000);
+                camera = new WebCamTexture(deviceCam.name, Mathf.FloorToInt(aspect * 1000), 1000);
                 break;
             }
-            
+
             if (SystemInfo.deviceType == DeviceType.Handheld && !deviceCam.isFrontFacing)
             {
                 float aspect = (float)(Screen.width / Screen.height);
@@ -58,7 +59,7 @@ public class CameraController : MonoBehaviour
 
     /** Activates the Camera
      * 
-     */ 
+     */
     public void ActivateCamera()
     {
         if (camera != null && !camera.isPlaying)
@@ -70,7 +71,7 @@ public class CameraController : MonoBehaviour
 
     /** Deactivates the Camera
      * 
-     */ 
+     */
     public void DeactivateCamera()
     {
         if (camera != null)
@@ -82,26 +83,52 @@ public class CameraController : MonoBehaviour
 
     /** Starts PhotoCoroutine
      * 
-     */ 
+     */
     public void TakePhoto()
     {
-        if(camAvailable && camera.isPlaying)
+        if (camAvailable && camera.isPlaying)
         {
             StartCoroutine(this.StartTakingPhotoCoroutine());
             this.DeactivateCamera();
         }
     }
-
+    //public byte[] ImageToByteArray(System.Drawing.Image imageIn)
+    //{
+    //    using (var ms = new MemoryStream())
+    //    {
+    //        imageIn.Save(ms, imageIn.RawFormat);
+    //        return ms.ToArray();
+    //    }
+    //}
+    public static Texture2D LoadPNG(string filePath) {
+ 
+    Texture2D tex = null;
+    byte[] fileData;
+ 
+    if (File.Exists(filePath))     {
+        fileData = File.ReadAllBytes(filePath);
+        tex = new Texture2D(2, 2);
+        tex.LoadImage(fileData); //..this will auto-resize the texture dimensions.
+    }
+    return tex;
+ }
     private IEnumerator StartTakingPhotoCoroutine()
     {
 
         RenderTexture renderTexture = new RenderTexture(camera.width, camera.height, 1);
         renderTexture.Create();
-        Graphics.Blit(camera, renderTexture);
+        string filePath = "C:/Users/Surlix/Documents/Figur3.png";
+        //change filepath pls
+        
+        
+        Texture2D tex2d = LoadPNG(filePath);
+        
+        Graphics.Blit(tex2d, renderTexture);
+        //Graphics.Blit(camera, renderTexture);
 
         Texture2D tmp = Util.ToTexture2D(renderTexture, RenderTexture.active);
 
-        this.capturedPhotoRaw.texture = Util.ToRenderTexture(Util.ResampleAndCrop(tmp,848, 848), RenderTexture.active);
+        this.capturedPhotoRaw.texture = Util.ToRenderTexture(Util.ResampleAndCrop(tmp, 848, 848), RenderTexture.active);
         yield return new WaitForEndOfFrame();
     }
 }
